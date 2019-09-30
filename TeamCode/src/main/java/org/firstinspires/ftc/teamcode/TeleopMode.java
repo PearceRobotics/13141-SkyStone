1c25a84c0a2d6de6198fa9b5823d40c24ab28a5d
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-09-30 17:30:04 -0500

    Attempt at making linearlift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -24,5 +24,17 @@ public class TeleopMode extends OpMode
     public void loop() {
         motorLeft.setPower(gamepad1.left_stick_y);
         motorRight.setPower(-gamepad1.right_stick_y);
+        if(gamepad1.left_bumper) {//up
+            motorLinearLiftLeft.setPower(1);
+            motorLinearLiftRight.setPower(-1);
+        }
+        else if(gamepad1.right_bumper) {//down
+            motorLinearLiftLeft.setPower(-1);
+            motorLinearLiftRight.setPower(1);
+        }
+        else {
+            motorLinearLiftRight.setPower(0);
+            motorLinearLiftLeft.setPower(0);
+        }
     }
 }
