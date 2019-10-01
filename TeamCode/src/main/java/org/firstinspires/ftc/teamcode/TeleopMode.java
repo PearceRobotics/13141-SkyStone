a0b6c8b25cd7a6f0f4b3d16413ff87ec5464e5aa
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-10-01 16:30:02 -0500

    Servo Arm Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Teleop", group = "Teleop" )
 public class TeleopMode extends OpMode
 {
@@ -13,12 +13,14 @@ public class TeleopMode extends OpMode
     private DcMotor motorRight;
     private DcMotor motorLinearLiftLeft;
     private DcMotor motorLinearLiftRight;
+    private Servo ArmServo;
     @Override
     public void init(){
         motorLeft = hardwareMap.get(DcMotor.class,"MotorLeft");
         motorRight = hardwareMap.get(DcMotor.class,"MotorRight");
         motorLinearLiftLeft = hardwareMap.get(DcMotor.class,"LinearLiftLeft");
         motorLinearLiftRight = hardwareMap.get(DcMotor.class,"LinearLiftRight");
+        ArmServo = hardwareMap.get(Servo.class,"ArmServo");
     }
     @Override
     public void loop() {
@@ -35,6 +37,12 @@ public class TeleopMode extends OpMode
         else {
             motorLinearLiftRight.setPower(0);
             motorLinearLiftLeft.setPower(0);
+        }
+        if(gamepad1.x){
+            ArmServo.setPosition(1);
+        }
+        else if(gamepad1.y){
+            ArmServo.setPosition(0);
         }
     }
 }
