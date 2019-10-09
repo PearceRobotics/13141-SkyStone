7bccd6b990a2b24929195092acab4651901ab15b
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-10-09 17:53:35 -0500

    Attempt at gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFowardRightStart.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFowardRightStart.java
@@ -1,39 +1,73 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name ="Drive Foward", group = "Beginner Auton")
-public class AutonDriveFowardRightStart extends LinearOpMode {
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name = "Drive Foward", group = "Beginner Auton")
+public class AutonDriveFowardRightStart extends OpMode {
     //Declare variables/ objects
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private ElapsedTime runtime = new ElapsedTime();
-
-
+    private BNO055IMU gyro;
+    Orientation lastAngles = new Orientation();
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
         motorLeft = hardwareMap.dcMotor.get("MotorLeft");
         motorRight = hardwareMap.dcMotor.get("MotorRight");
-
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-        
-        waitForStart();
-        runtime.reset();
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        gyro = hardwareMap.get(BNO055IMU.class, "Gyro");
+        gyro.initialize(parameters);
+        resetAngle();
 
-        while (opModeIsActive() && runtime.seconds() < 1.0) {
-            driveForward(1.0);
-        }
-        while (opModeIsActive() && runtime.seconds() > 1.0) {
-            driveForward(0.0);
-        }
     }
-        public void driveForward ( double power)
-        {
-            motorLeft.setPower(power);
-            motorRight.setPower(power);
-        }
+
+    private void resetAngle() {
+        lastAngles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+    }
+
+    private double getAngel() {
+        Orientation angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+        lastAngles = angles;
+        return deltaAngle;
+    }
+
+    @Override
+    public void loop() {
+        driveStraight();
+    }
+
+    public void arcadeDrive(double StraightPower, double turnModifer) {
+        motorLeft.setPower(StraightPower + turnModifer);
+        motorRight.setPower(-(StraightPower - turnModifer));
+    }
+
+    public void driveStraight() {
+        double error = getAngel();
+        double turnPower = error * .01;
+        arcadeDrive(1.0,turnPower);
+
+    }
+
+
     }
