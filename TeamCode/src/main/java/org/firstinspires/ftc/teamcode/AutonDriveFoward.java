0a6733679224846f167e4074d55ecd03789d4467
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-10-17 18:00:55 -0500

    Auton testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFoward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFoward.java
@@ -11,25 +11,46 @@ public class AutonDriveFoward extends LinearOpMode {
     //Declare variables/ objects
     private DcMotor motorLeft;
     private DcMotor motorRight;
+    private Servo ArmServo;
     private ElapsedTime runtime = new ElapsedTime();
+    double x = 4.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
         motorLeft = hardwareMap.dcMotor.get("MotorLeft");
         motorRight = hardwareMap.dcMotor.get("MotorRight");
+        ArmServo = hardwareMap.get(Servo.class,"ArmServo");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
         
         waitForStart();
         runtime.reset();
 
-        while (opModeIsActive() && runtime.seconds() < 1.0) {
+        while (opModeIsActive() && runtime.seconds() < x) {
             driveForward(1.0);
         }
-        while (opModeIsActive() && runtime.seconds() > 1.0 && runtime.seconds() <   1.5) {
+        while (opModeIsActive() && runtime.seconds() > x && runtime.seconds() <   x + 0.7) {
             driveForward(0.0);
             motorRight.setPower(1);
             motorLeft.setPower(-.9);
+        }
+        x = x + 0.7;
+        while (opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 1.0)
+        {
+            driveForward(1.0);
+        }
+        x = x + 1;
+        while (opModeIsActive() && runtime.seconds() > x  && runtime.seconds() < x + 0.1) {
+            driveForward(0.0);
+            ArmServo.setPosition(1);
+        }
+        x = x + 0.1;
+        while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 1.8) {
+            driveForward(-1.0);
+        }
+        x = x + 1.8;
+        while( opModeIsActive() && runtime.seconds() > x && runtime.seconds()< x + 0.1) {
+        ArmServo.setPosition(0);
         }
 
     }
