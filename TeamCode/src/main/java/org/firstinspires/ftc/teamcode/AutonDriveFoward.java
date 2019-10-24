123a323215e980d1dfd453c055d61e6859da02d5
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-10-24 18:22:52 -0500

    changes to use lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFoward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveFoward.java
@@ -12,56 +12,59 @@ public class AutonDriveFoward extends LinearOpMode {
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private Servo ArmServo;
+    private DcMotor motorLinearLiftLeft;
+    private DcMotor motorLinearLiftRight;
+
     private ElapsedTime runtime = new ElapsedTime();
-    double x = 2.0;
+    double x = 0.3;
 
     @Override
     public void runOpMode() throws InterruptedException {
         motorLeft = hardwareMap.dcMotor.get("MotorLeft");
         motorRight = hardwareMap.dcMotor.get("MotorRight");
         ArmServo = hardwareMap.get(Servo.class,"ArmServo");
+        motorLinearLiftLeft = hardwareMap.get(DcMotor.class,"LinearLiftLeft");
+        motorLinearLiftRight = hardwareMap.get(DcMotor.class,"LinearLiftRight");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
         
         waitForStart();
         runtime.reset();
-
-        while (opModeIsActive() && runtime.seconds() < x) {
-            driveForward(1.0);
-        }
-        while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + .5) {
-            motorRight.setPower(1);
-            motorLeft.setPower(-.9);
+        while(opModeIsActive() && runtime.seconds() < x) {
+            motorLinearLiftLeft.setPower(1);
+            motorLinearLiftRight.setPower(-1);
         }
 
-        x = x + .5;
         while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 2)
         { driveForward(1);
         }
         x = x + 2;
-        while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 5)
+        while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 0.3)
         {
             motorLeft.setPower(0);
             motorRight.setPower(0);
-            ArmServo.setPosition(1);
+            motorLinearLiftLeft.setPower(-1);
+            motorLinearLiftRight.setPower(1);
         }
-        x = x + 5;
+        x = x + .3;
         while( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 2){
-            motorLeft.setPower(0.9);
+            motorLeft.setPower(0.85);
             motorRight.setPower(1);
         }
         x = x + 2;
-        while ( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 1){
-            ArmServo.setPosition(0);
+        while ( opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + .3){
+            motorLinearLiftLeft.setPower(1);
+            motorLinearLiftRight.setPower(-1);
         }
+        x = x + .3;
     }
         public void driveForward ( double power) {
             motorRight.setPower(-power);
 
             if (power - .1 <= 0) {
-                motorLeft.setPower(-.1);
+                motorLeft.setPower(-.15);
             } else{
-                motorLeft.setPower(-(power-.1));
+                motorLeft.setPower(-(power-.15));
 }
         }
     }
