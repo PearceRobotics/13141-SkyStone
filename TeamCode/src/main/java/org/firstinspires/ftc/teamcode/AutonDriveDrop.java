1d26acd78c7e52fa8c6095d493973e126081d5fa
yuming9 <55634987+yuming9@users.noreply.github.com>
2019-11-22 18:26:09 -0600

    Update AutonDriveDrop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveDrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonDriveDrop.java
@@ -6,42 +6,97 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name ="Drop Arm", group = "Beginner Auton")
+@Autonomous(name ="DriveDrop", group = "Beginner Auton")
 public class AutonDriveDrop extends LinearOpMode {
     //Declare variables/ objects
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
+    private DcMotor MotorLeftFront;
+    private DcMotor MotorRightFront;
+    private DcMotor MotorLeftBack;
+    private DcMotor MotorRightBack;
     private Servo ArmServo;
-    private DcMotor motorLinearLiftLeft;
-    private DcMotor motorLinearLiftRight;
+    private Servo ServoBack1;
+    private Servo ServoBack2;
+    //  private DcMotor motorLinearLiftLeft;
+    // private DcMotor motorLinearLiftRight;
+    private Servo CapstoneArm;
 
     private ElapsedTime runtime = new ElapsedTime();
-    double x = 5;
+    double x = .3 ;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motorLeft = hardwareMap.dcMotor.get("MotorLeft");
-        motorRight = hardwareMap.dcMotor.get("MotorRight");
+        MotorLeftBack = hardwareMap.dcMotor.get("MotorLeftBack");
+        MotorLeftFront = hardwareMap.dcMotor.get("MotorLeftFront");
+        MotorRightBack = hardwareMap.dcMotor.get("MotorRightBack");
+        MotorRightFront = hardwareMap.dcMotor.get("MotorRightFront");
         ArmServo = hardwareMap.get(Servo.class, "ArmServo");
-        motorLinearLiftLeft = hardwareMap.get(DcMotor.class, "LinearLiftLeft");
-        motorLinearLiftRight = hardwareMap.get(DcMotor.class, "LinearLiftRight");
+        ServoBack1 = hardwareMap.get(Servo.class, "ServoBack1");
+        ServoBack2 = hardwareMap.get(Servo.class, "ServoBack2");
+        CapstoneArm = hardwareMap.get(Servo.class, "CapstoneArm");
+        //  motorLinearLiftLeft = hardwareMap.get(DcMotor.class, "LinearLiftLeft");
+        //   motorLinearLiftRight = hardwareMap.get(DcMotor.class, "LinearLiftRight");
 
-        motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         runtime.reset();
-        while (opModeIsActive() && runtime.seconds() < x) {
-            ArmServo.setPosition(1);
+        while(opModeIsActive() && runtime.seconds() < x) {
+            Foward();
+        }
+        while(opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 1.4) {
+        StrafeLeft();
+        }
+        x=x+1.4;
+        while(opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 3){
+            MotorLeftBack.setPower(0);
+            MotorLeftFront.setPower(0);
+            MotorRightBack.setPower(0);
+            MotorRightFront.setPower(0);
+        }
+        x=x+3;
+        while(opModeIsActive() && runtime.seconds() > x && runtime.seconds() < x + 3){
+            {
+                ArmServo.setPosition(1);
+            }
+            x=x+3;
         }
 
-        }
-        public void driveForward ( double power) {
-            motorRight.setPower(-power);
 
-            if (power - .1 <= 0) {
-                motorLeft.setPower(-.15);
-            } else{
-                motorLeft.setPower(-(power-.15));
-}
-        }
+
     }
+    public void StrafeLeft()
+    {
+        MotorRightBack.setPower(.80);
+        MotorLeftBack.setPower(.70);
+        MotorLeftFront.setPower(-.70);
+        MotorRightFront.setPower(-.80);
+    }
+    public void StrafeRight()
+    {
+        MotorRightBack.setPower(-.20);
+        MotorLeftBack.setPower(-.10);
+        MotorLeftFront.setPower(.10);
+        MotorRightFront.setPower(.20);
+
+    }
+    public void Foward()
+    {
+        MotorRightBack.setPower(-.9);
+        MotorLeftBack.setPower(1);
+        MotorLeftFront.setPower(1);
+        MotorRightFront.setPower(-.9);
+    }
+    public void Backward(){
+        MotorRightBack.setPower(.30);
+        MotorLeftBack.setPower(-.25);
+        MotorLeftFront.setPower(-.25);
+        MotorRightFront.setPower(.30);
+    }
+    public void stops(){
+        MotorLeftFront.setPower(0);
+        MotorRightBack.setPower(0);
+        MotorLeftBack.setPower(0);
+        MotorRightFront.setPower(0);
+    }
+}
+
+
